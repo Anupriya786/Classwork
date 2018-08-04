5027705dc9fced27edf8ed8ab16cfd9320849986
saravanan <saravanaan@gmail.com>
2018-08-04 18:05:59 +0530

    changes in sysout

--- src/main/java/day1/FirstProgram.java
+++ src/main/java/day1/FirstProgram.java
@@ -12,6 +12,8 @@ FirstProgram o1 = new FirstProgram();
 FirstProgram o2 = new FirstProgram();
 myAge=o2.myAge; // changed by abhinaya
 //System.out.println(o2.myAge);
+System.out.println("Saravanan");
+
 	}
 
 }
